c6a81efdab8246301504d29479d99d9b07b23901
VIVIAN BASSEY <vivianbasseya@gmail.com>
2024-07-28 22:13:07 +0100

    my active portfolio

---
+++ index.js
@@ -0,0 +1,20 @@
+let tablinks =document.getElementsByClassName("tab-links")
+let tabcontents = document.getElementsByClassName("tab-contents")
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link')
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab')
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+let sidemenu = document.getElementById("sidemenu")
+function closeMenu(){
+    sidemenu.style.right = "-200px"
+}
+function openMenu(){
+    sidemenu.style.right = "0"
+}
